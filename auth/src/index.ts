c48bbfe4e17b8c6120d961e9de420b6337c608aa
Marwen.eshili <marweneshili@takiacademyteam.com>
2023-03-30 10:55:09 +0100

    feat: second commit

---
+++ auth/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('*', (req, res) => {
+  res.send({ body: {} });
+});
+
+app.listen(3000, () => {
+  console.log('Listining on port 3000');
+});
